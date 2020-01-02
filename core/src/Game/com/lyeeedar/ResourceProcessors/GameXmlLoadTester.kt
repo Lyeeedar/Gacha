a1840a8fa4297fc5959059a0af3bcd92d5a4a58b
lyeeedar <lyeeedar@gmail.com>
2020-01-02 06:05:19 +0000

    Move the resource compilers into the Engine

---
+++ core/src/Game/com/lyeeedar/ResourceProcessors/GameXmlLoadTester.kt
@@ -0,0 +1,18 @@
+package com.lyeeedar.ResourceProcessors
+
+import com.lyeeedar.Util.XmlData
+
+class GameXmlLoadTester
+{
+	companion object
+	{
+		fun testLoad(xml: XmlData)
+		{
+			when (xml.name.toUpperCase())
+			{
+				//"EFFECT" -> ParticleEffect.load(path.split("Particles/")[1], ParticleEffectDescription(""))
+				//else -> throw RuntimeException("Unhandled path type '${xml.name}'!")
+			}
+		}
+	}
+}
